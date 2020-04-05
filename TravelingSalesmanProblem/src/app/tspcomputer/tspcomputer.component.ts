218109e56a42f27fe40c8373e1a7e4480c17b868
daniel r <d.randrianjatovo@gmail.com>
2020-04-05 16:21:12 +0200

    naive getTableDisntanc

--- TravelingSalesmanProblem/src/app/tspcomputer/tspcomputer.component.ts
+++ TravelingSalesmanProblem/src/app/tspcomputer/tspcomputer.component.ts
@@ -9,10 +9,14 @@ import {GeoPoint} from "../interfaces/geopoint";
 export class TSPComputerComponent implements OnInit {
 
   @Input() table: GeoPoint[];
-  @Input() temperature:any;
-  @Input() decreaseRate:any;
-  @Input() time:any;
-
+  @Input() temperature:number;
+  @Input() decreaseRate:number;
+  @Input() time:number;
+  private p1: GeoPoint = {"latitude": 0, "name": "point1", "longitude": 0};
+  private p2: GeoPoint = {"latitude": 1, "name": "point2", "longitude": 1};
+  private nameTable = [];
+  private longitudeTable:number[] = [];
+  private latitudeTable:number[] = [];
 
   constructor() { }
 
@@ -20,6 +24,36 @@ export class TSPComputerComponent implements OnInit {
     this.temperature = 0.0
     this.time = 0.0
     this.decreaseRate = 0.0
+
+    console.log(this.splitTable(this.table))
+    console.log(this.getTableDistance(this.longitudeTable,this.latitudeTable))
+
   }
+
+  splitTable(table:GeoPoint[]){
+    table.forEach(x => {
+      this.nameTable.push(x.name)
+      this.longitudeTable.push(x.longitude)
+      this.latitudeTable.push(x.latitude)
+    })
+
+  }
+  /*
+  getDistance(p1:GeoPoint,p2:GeoPoint){
+    return ((Math.abs(p1.longitude-p2.longitude)**2)+(Math.abs(p1.longitude-p2.latitude)**2))**(1/2)
+  }
+
+  getRoadDistance(table:GeoPoint[]){
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    return table.reduce(reducer)
+  }
+  */
+
+  getTableDistance(a:number[], b:number[]) {
+    return a.map((x, i) => Math.abs( x - b[i] ) ** 2).reduce((sum, now) => sum + now) ** (1/2)
+  }
+
+
+
 
 }
