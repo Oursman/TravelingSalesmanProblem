490ee4b3f62158b382ed91a8bd9b3681db77ba22
daniel r <d.randrianjatovo@gmail.com>
2020-04-05 17:31:55 +0200

    mohamed tu vas travailler

--- TravelingSalesmanProblem/src/app/tspcomputer/tspcomputer.component.ts
+++ TravelingSalesmanProblem/src/app/tspcomputer/tspcomputer.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {GeoPoint} from "../interfaces/geopoint";
 
 @Component({
@@ -8,6 +8,7 @@ import {GeoPoint} from "../interfaces/geopoint";
 })
 export class TSPComputerComponent implements OnInit {
 
+  @Output() tableChange = new EventEmitter<GeoPoint[]>();
   @Input() table: GeoPoint[];
   @Input() temperature:number;
   @Input() decreaseRate:number;
@@ -26,6 +27,8 @@ export class TSPComputerComponent implements OnInit {
     this.temperature = 0.0
     this.time = 0.0
     this.decreaseRate = 0.0
+    let newTable =  Object.assign({},this.table);
+    /*
     this.start = this.table[0];
     this.end = this.table[this.table.length-1];
 
@@ -44,17 +47,31 @@ export class TSPComputerComponent implements OnInit {
 
     console.log(this.splitTable(this.table))
     console.log(this.getTableDistance(this.longitudeTable,this.latitudeTable))
+    console.log(this.randomSwap())
+    */
+
+    console.log(this.getTableDistance(this.table))
+    console.log(this.getTableDistance(this.randomSwap(newTable)))
 
   }
 
+  /*
   splitTable(table:GeoPoint[]){
     table.forEach(x => {
       this.nameTable.push(x.name)
       this.longitudeTable.push(x.longitude)
       this.latitudeTable.push(x.latitude)
     })
-
+  }*/
+  splitTable(table:GeoPoint[],a:number[],b:number[]){
+    table.forEach(x => {
+      this.nameTable.push(x.name)
+      a.push(x.longitude)
+      b.push(x.latitude)
+    })
   }
+
+
   /*
   getDistance(p1:GeoPoint,p2:GeoPoint){
     return ((Math.abs(p1.longitude-p2.longitude)**2)+(Math.abs(p1.longitude-p2.latitude)**2))**(1/2)
@@ -65,13 +82,42 @@ export class TSPComputerComponent implements OnInit {
     return table.reduce(reducer)
   }
   */
-
+  /*
   getTableDistance(a:number[], b:number[]) {
+    return a.map((x, i) => Math.abs( x - b[i] ) ** 2).reduce((sum, now) => sum + now) ** (1/2)
+  }*/
+
+  getTableDistance(table:GeoPoint[]) {
+    let a:number[]=[]  // SI TA UN PROBLEME  = mets en variable COMPOSANT this.a etc..
+    let b:number[]=[]
+    this.splitTable(table,a,b)
     return a.map((x, i) => Math.abs( x - b[i] ) ** 2).reduce((sum, now) => sum + now) ** (1/2)
   }
 
+  getRandomInt(max:number) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
+  randomSwap(table:GeoPoint[]){
+    console.log(table)
+    console.log(table.length)
+    let i1 = this.getRandomInt(table.length-1)
+    let i2 = this.getRandomInt(table.length-1)
 
+    console.log("first point to swap "+table[i1])
+    console.log("second point to swap "+table[i2])
+
+    let tmp = table[i1]
+    this.table[i1] = table[i2]
+    this.table[i2] = tmp
+
+    return table
+
+    /*
+    let clonedPoint = Object.assign({},table); // copie superficielle (suffisante ici pour éviter de modifier accidentellement le point)
+    this.tableChange.emit(clonedPoint);*/
+
+  }
 
   onTemperatureChange(e) {
     this.temperature =e.target.value;
